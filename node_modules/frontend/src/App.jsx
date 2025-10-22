6827758d130e14373e6018adc3a1dbf5a42070c5
nick <nickswanenberg@gmail.com>
2025-10-22 15:37:30 +0200

    Frontend setup

---
+++ node_modules/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h1>TikTok Giveaway Widget 🎁</h1>
+      <p>Frontend is running successfully!</p>
+    </div>
+  );
+}
+
+export default App;
+
